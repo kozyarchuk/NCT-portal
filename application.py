70a233eb942890f7cc6cca03d49c83a09c02d2e5
kozyarchuk <maksim_kozyarchuk@yahoo.com>
2015-04-30 17:09:20 -0400

    add basic messaging

--- application.py
+++ application.py
@@ -1,12 +1,44 @@
 import flask
 from flask.templating import render_template
+from flask import Flask, render_template, jsonify, request
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+import boto.sqs
+from boto.sqs.message import Message
 
 application = flask.Flask(__name__)
+application.secret_key = 'Secret'
+
+_boto_conn = None
+def get_boto_conn():
+    global _boto_conn
+    if not _boto_conn:
+        _boto_conn = boto.sqs.connect_to_region("us-east-1")
+
+    return  _boto_conn
+
+class FilesForm(Form):
+    file_name = StringField('file_name', validators=[DataRequired()])
 
 @application.route('/')
 @application.route('/index.html')
-def hello_world():
-    return render_template('index.html', name="Travis-CI")
+def index():
+    return render_template('index.html' )
+
+
+@application.route('/files.html', methods=['GET', 'POST'])
+def files():
+    if request.method == 'GET':
+        form = FilesForm()
+        return render_template('files.html', form=form)
+    else:
+        conn = get_boto_conn()
+        q = conn.get_queue('NCT-service-request')
+        m = Message()
+        m.set_body('The file is on its way.')
+        q.write(m)
+        return "Message Sent"
 
 if __name__ == '__main__':
     application.debug=True
--- tests.py
+++ tests.py
@@ -3,20 +3,30 @@ from application import application as app
 
 class TestApplication(unittest.TestCase):
 
-    def test_index_route(self):
+    def setUp(self):
         app.debug = True
-        c = app.test_client()
+        self.c = app.test_client()
 
-        rv = c.get('/')
+    def test_index_route(self):
+
+        rv = self.c.get('/')
         assert "NCT PORTAL" in rv.data.decode("utf-8")
 
-        rv = c.get('/index.html')
+        rv = self.c.get('/index.html')
         assert "NCT PORTAL" in rv.data.decode("utf-8")
 
     def test_static_route(self):
-        app.debug = True
-        c = app.test_client()
 
-        rv = c.get('static/css/animate.css')
+        rv = self.c.get('static/css/animate.css')
 
         self.assertEquals(200,  rv.status_code)
+
+    def test_files_route(self):
+
+        rv = self.c.get('/files.html')
+        assert "Upload File" in rv.data.decode("utf-8")
+
+    # def test_files_route(self):
+    #
+    #     rv = self.c.post('/files.html')
+    #     assert "Message Sent" in rv.data.decode("utf-8")
